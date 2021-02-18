98ab625184af2ed6ac84242c2c854d664d257176
Matthew Chaleski <chaleski76@gmail.com>
2021-02-18 10:28:48 -0500

    working js file

---
+++ index.js
@@ -0,0 +1,32 @@
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("test")
+    document.querySelector('.btn').addEventListener('click', function(e) {
+        e.preventDefault()
+        const searchString = document.querySelector('.search').value
+        const urlEncodedSearchString = encodeURIComponent(searchString);
+        fetch('https://api.rainforestapi.com/request?api_key=D323AD48C3F24BBB8F957FB6024D62C4&type=search&amazon_domain=amazon.com&search_term=' + urlEncodedSearchString + '&sort_by=featured' )
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                const searchResults = document.querySelector('.col-sm')
+                let resultsHTML = renderSearches(data.search_results)
+                searchResults.innerHTML = resultsHTML;
+                searchData = data.Search
+            })
+    })
+})
+
+function renderSearches(resultsArray) {
+    const resultHtmlArray = resultsArray.map(function(currentResult) {
+        return `<div>
+            <h2>${currentResult.title}<h2>
+            <img src="${currentResult.image}" class="picture" alt="product image">
+            <div>`
+    });
+        return resultHtmlArray.join('');
+}
